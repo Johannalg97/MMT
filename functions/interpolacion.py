dd1832f131ff5fabf54f74f9cc4a165e438de8b7
DAndres12 <dpena@tvmarkets.com>
2022-10-05 20:57:12 -0500

    funcyion listas actualizada

--- functions/interpolacion.py
+++ functions/interpolacion.py
@@ -1,17 +1,31 @@
 import numpy as np
 
-def recibir_lista(Lista_de_tuplas):
-    """Esta funcion recibe la lista de tuplas [(Lambda, n , k),...] de cada archivo 
-    que eliga el usario, para separar, lambda, n y k por listas"""
-    Lambda = []
+def recibir_lista(lista_de_tuplas):
+    """
+    Esta funcion recibe la lista de tuplas [(Lambda, n , k),...] de cada archivo 
+    que eliga el usario, para separar, lambda, n y k por listas
+
+    lista_de_tuplas - list []
+
+    return:
+    """
+    w = []
     n = []
     k = []
-    for tupla in Lista_de_tuplas:
-        Lambda.append (tupla[0])
-        n.append (tupla [1])
-        k.append (tupla [2])
-    return [Lambda, n, k]
+    
+    for i in lista_de_tuplas:
+        if len(i) == 2:
+            w.append(i[0])
+            n.append(i[1])
+            k.append(0)
 
+        elif len(i) == 3:
+            w.append(i[0])
+            n.append(i[1])
+            k.append(i[2])
+    
+    return w, n, k 
+    
 def interpolacion(Lista, Rango):
     """Esta funcion me realiza la interpolaciòn  """
     X = np.linspace (Rango [0], Rango [1], 1)
@@ -24,11 +38,12 @@ def interpolacion(Lista, Rango):
 
 #prueba
 if __name__ == '__main__':
-    b = [(1, 2, 3), (2, 4, 6), (3, 6, 9)]
+    b = [(1, 2), (2, 4), (3, 6)]
     c = recibir_lista(b)
     print(c)
-    d = interpolacion(c, [1.5, 1.5])
-    print(d)
+    print(type(c))
+    #d = interpolacion(c, [1.5, 1.5])
+    #print(d)
 
 
     
